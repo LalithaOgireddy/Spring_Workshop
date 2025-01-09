6b30aba9b10f6454b3a9d3367196da645bd470f9
Lalitha Ogireddy <lalitha.ogireddy@gmail.com>
2025-01-09 12:13:38 +0100

    Part 2

--- src/main/java/com/lalitha/Main.java
+++ src/main/java/com/lalitha/Main.java
@@ -2,7 +2,10 @@ package com.lalitha;
 
 import com.lalitha.config.ComponentScanConfig;
 import com.lalitha.data.StudentDao;
+import com.lalitha.util.ScannerInputService;
+import com.lalitha.util.UserInputService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -11,5 +14,6 @@ public class Main {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
         StudentDao studentDao = context.getBean(StudentDao.class);
+        UserInputService userInputService=context.getBean(ScannerInputService.class);
     }
 }
---
+++ src/main/java/com/lalitha/util/UserInputService.java
@@ -0,0 +1,6 @@
+package com.lalitha.util;
+
+public interface UserInputService {
+    String getString();
+    int getInt();
+}
